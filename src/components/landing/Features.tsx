e531c231d0cdf5499c415867c567ace58e37581b
Upeka-Wihanga <upeka.20231250@iit.ac.lk>
2025-02-24 23:28:21 +0530

    Removed Second Enter

--- src/components/landing/Features.tsx
+++ src/components/landing/Features.tsx
@@ -129,7 +129,6 @@ const Features = () => {
     const itemVariants = {
         hidden: { opacity: 0, y: 20 },
         visible: {
-            
             opacity: 1,
             y: 0
         }
